c0b378108468f4ec9018c93cf2d2895896fcd5a1
Pavel Klos <pavelkloscz@gmail.com>
2023-06-08 23:38:37 +0200

    visitor authentication scheme

--- AuthenticationSchemas/Program.cs
+++ AuthenticationSchemas/Program.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System.Security.Claims;
+using System.Text.Encodings.Web;
 
 // *****************************************************************************
 var builder = WebApplication.CreateBuilder(args);
 // *****************************************************************************
 // Add services to the container.
-builder.Services.AddAuthentication()    // [+] Authentication services
-    .AddCookie("local");                //     authentication scheme = 'local'
-builder.Services.AddAuthorization();    // [+] Authorization Policy services
+builder.Services.AddAuthentication()        // [+] Authentication services
+    //.AddCookie("visitor")                 //     authentication scheme = 'visitor'
+    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", o => {})                   
+    .AddCookie("local");                    //     authentication scheme = 'local'
+builder.Services.AddAuthorization(b =>      // [+] Authorization Policy services
+{
+    b.AddPolicy("customer", p =>
+    {
+        p.AddAuthenticationSchemes("local", "visitor")
+            .RequireAuthenticatedUser();
+    });
+});
 
 // *****************************************************************************
 var app = builder.Build();
@@ -21,13 +33,15 @@ app.UseAuthorization();     // [+] Authorization to HTTP application pipeline
 // *****************************************************************************
 // Minimal API
 // *****************************************************************************
-app.MapGet("/", (ctx) => Task.FromResult("Hello World!"));
+app.MapGet("/", (ctx) => Task.FromResult("Hello World!"))
+    .RequireAuthorization("customer");
 
 app.MapGet("/login-local", async (ctx) =>
 {
+    // 'LOCAL' SCHEME
     var claims = new List<Claim>();
     claims.Add(new Claim("usr", "anton"));
-    var identity = new ClaimsIdentity(claims, "cookie");
+    var identity = new ClaimsIdentity(claims, "local");
     var user = new ClaimsPrincipal(identity);
 
     // Sign in principal for specified scheme
@@ -37,3 +51,34 @@ app.MapGet("/login-local", async (ctx) =>
 // *****************************************************************************
 app.Run();
 // *****************************************************************************
+
+public class VisitorAuthHandler : CookieAuthenticationHandler
+{
+    public VisitorAuthHandler(
+        IOptionsMonitor<CookieAuthenticationOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock)
+        : base(options, logger, encoder, clock)
+    {
+
+    }
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var result = await base.HandleAuthenticateAsync(); // AuthenticateResult
+        if (result.Succeeded)
+        {
+            return result;
+        }
+
+        // 'VISITOR' SCHEME
+        var claims = new List<Claim>();
+        claims.Add(new Claim("usr", "anton"));
+        var identity = new ClaimsIdentity(claims, "visitor");
+        var user = new ClaimsPrincipal(identity);
+
+        await Context.SignInAsync("visitor", user);
+
+        return AuthenticateResult.Success(new AuthenticationTicket(user, "visitor"));
+    }
+}
