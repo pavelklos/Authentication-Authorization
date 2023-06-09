7023e2ce6c85f1f336a00ee762389b228884a572
Pavel Klos <pavelkloscz@gmail.com>
2023-06-09 13:19:55 +0200

    patreon authentication scheme

--- AuthenticationSchemas/Program.cs
+++ AuthenticationSchemas/Program.cs
@@ -10,14 +10,41 @@ var builder = WebApplication.CreateBuilder(args);
 // *****************************************************************************
 // Add services to the container.
 builder.Services.AddAuthentication()        // [+] Authentication services
-    //.AddCookie("visitor")                 //     authentication scheme = 'visitor'
-    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", o => {})                   
-    .AddCookie("local");                    //     authentication scheme = 'local'
+                                            //.AddCookie("visitor")                 //     authentication scheme = 'visitor'
+    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", o => { })
+    .AddCookie("local")                     // authentication scheme = 'local'
+    .AddCookie("patreon-cookie")            // authentication scheme = 'patreon-cookie'
+    .AddOAuth("external-patreon", o =>      // Adds OAuth 2.0 based authentication using specified authentication scheme
+    {
+        o.SignInScheme = "patreon-cookie";
+
+
+        o.ClientId = "id";
+        o.ClientSecret = "secret";
+
+        // Mock OAuth2 / OpenID Connect Login
+        // - https://oauth.mocklab.io/
+        o.AuthorizationEndpoint = "https://oauth.mocklab.io/oauth/authorize";
+        o.TokenEndpoint = "https://oauth.mocklab.io/oauth/token";
+        o.UserInformationEndpoint = "https://oauth.mocklab.io/userinfo";
+
+        o.CallbackPath = "/cb-patreon";
+
+        o.Scope.Add("profile");
+        o.SaveTokens = true;
+    });
 builder.Services.AddAuthorization(b =>      // [+] Authorization Policy services
 {
+    // customer = local, visitor
     b.AddPolicy("customer", p =>
     {
-        p.AddAuthenticationSchemes("local", "visitor")
+        p.AddAuthenticationSchemes("patreon-cookie", "local", "visitor")
+            .RequireAuthenticatedUser();
+    });
+    // user = local
+    b.AddPolicy("user", p =>
+    {
+        p.AddAuthenticationSchemes("local")
             .RequireAuthenticatedUser();
     });
 });
@@ -47,6 +74,15 @@ app.MapGet("/login-local", async (ctx) =>
     // Sign in principal for specified scheme
     await ctx.SignInAsync("local", user); // authentication scheme = 'local' 
 });
+
+app.MapGet("/login-patreon", async (ctx) =>
+{
+    // SIGN-IN EXTERNALLY BY 'OAuth2'
+    await ctx.ChallengeAsync("external-patreon", new AuthenticationProperties()
+    {
+        RedirectUri = "/"
+    });
+}).RequireAuthorization("user");
 
 // *****************************************************************************
 app.Run();
